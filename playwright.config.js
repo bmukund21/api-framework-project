67931d737082fb7496ee644defb962cd8d33b338
Bal Mukund <bmukund.official@gmail.com>
2024-11-07 00:06:16 +0400

    changed report structure

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  reporter: [['html', { outputFile: 'test-results/report.html' }]],
+  reporter: [['list']],
   // Other configurations like projects, timeout, etc.
   workers: 4, // Number of parallel workers (adjust as needed)
   testDir: './tests', // Directory where test files are located
