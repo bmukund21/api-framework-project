7508ecc280499b37bae4bc4d4925a66a23832918
Bal Mukund <bmukund.official@gmail.com>
2024-11-06 18:32:33 +0400

    comented home page test file

--- tests/homePageTest.spec.js
+++ tests/homePageTest.spec.js
@@ -1,25 +1,25 @@
-import { test, expect } from '@playwright/test';
-import { extractTokens, getAuthToken } from '../helper/authHelper.js'; // Updated imports
-import { HOME_PAGE_SLUG } from '../config/config.js';
+// import { test, expect } from '@playwright/test';
+// import { extractTokens, getAuthToken } from '../helper/authHelper.js'; // Updated imports
+// import { HOME_PAGE_SLUG } from '../config/config.js';
 
-test.beforeAll(async ({ request }) => {
-  await extractTokens(request); // Extract tokens before the tests
-});
+// test.beforeAll(async ({ request }) => {
+//   await extractTokens(request); // Extract tokens before the tests
+// });
 
-// Test home page
-test('home page get request', async ({ request }) => {
-  const authToken = getAuthToken();  // Retrieve the auth token
-  const response = await request.get(HOME_PAGE_SLUG, {
-    headers: {
-      'x-auth-token': authToken,  // Pass the token in the x-auth-token header
-    },
-  });
-  const responseBody = await response.json();
+// // Test home page
+// test('home page get request', async ({ request }) => {
+//   const authToken = getAuthToken();  // Retrieve the auth token
+//   const response = await request.get(HOME_PAGE_SLUG, {
+//     headers: {
+//       'x-auth-token': authToken,  // Pass the token in the x-auth-token header
+//     },
+//   });
+//   const responseBody = await response.json();
   
-  // Log the response status and body
-  console.log('Response Status:', response.status());
+//   // Log the response status and body
+//   console.log('Response Status:', response.status());
 
-  expect(response.status()).toBe(200);
-  expect(responseBody).toHaveProperty('type');
-  expect(responseBody).toHaveProperty('slug');
-});
+//   expect(response.status()).toBe(200);
+//   expect(responseBody).toHaveProperty('type');
+//   expect(responseBody).toHaveProperty('slug');
+// });
